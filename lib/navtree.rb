04a5d35c17c9a56a3a0eb470f8046760d3525f41
Alexander Shabunevich <loonar@gmail.com>
2012-10-25 22:03:26 +0400

    Fix missing section (helpers).

--- lib/navtree.rb
+++ lib/navtree.rb
@@ -2,15 +2,24 @@ def navtree(level)
   ret = Hash.new
 
   for item in @items
-    if item[:classnames] and item[:classnames][1] == level and item[:framework] and item[:stylesheet]
-      mixins = mixins(item)
-      for mixin in mixins
-        ret = navtree_build(ret, item, mixin, "mixin")
+    if item[:classnames] and item[:classnames][1] == level and item[:framework]
+      # get references from code
+      if item[:stylesheet]
+        mixins = mixins(item)
+        for mixin in mixins
+          ret = navtree_build(ret, item, mixin, "mixin")
+        end
+
+        functions = functions(item)
+        for func in functions
+          ret = navtree_build(ret, item, func, "function")
+        end
       end
 
-      functions = functions(item)
-      for func in functions
-        ret = navtree_build(ret, item, func, "function")
+      if item[:documented_functions]
+        for func in item[:documented_functions]
+          ret = navtree_build(ret, item, func)
+        end
       end
     end
   end
@@ -20,7 +29,7 @@ def navtree(level)
 end
 
 # helper function for navtree
-def navtree_build(ret, item, el, el_type = "mixin")
+def navtree_build(ret, item, el, el_type = "")
   module_name = item[:classnames][2] ? item[:classnames][2] : item[:classnames][1];
   page_name = item[:crumb]
 
@@ -38,8 +47,9 @@ def navtree_build(ret, item, el, el_type = "mixin")
     #if item[:deprecated] ret[module_name][:deprecated] = true
   end
 
-  ret[module_name][:children][page_name][:children][el.name] = {
-    :signature => el.sass_signature(:none),
+  el_name = el.respond_to?(:name) ? el.name : el
+  ret[module_name][:children][page_name][:children][el_name] = {
+    :signature => el.respond_to?(:sass_signature) ? el.sass_signature(:none) : el,
     :el_type => el_type
   }
 
